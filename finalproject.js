c9a8867a73214493fc11588b46ae005f3a121e1d
brader0 <brader01@luther.edu>
2015-10-19 14:10:00 -0500

    Create finalproject.js

---
+++ finalproject.js
@@ -0,0 +1,13 @@
+addTask = function() {
+  article = document.querySelector("theArticle");
+  uL = document.createElement("ul");
+  newLi = document.createElement("li");
+  lIcontents = document.createTextNode(This is first");
+        li2 = document.createElement("li");
+        li2contents = document.createTextNode("This is second");
+        li2.appendChild(li2contents);
+        li1.appendChild(li1contents);
+        li1.appendChild(li2);
+        oL.appendChild(li1);
+        body.appendChild(oL);
+      }
