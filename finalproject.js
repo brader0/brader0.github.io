b38922254791252c2a99065c3a1ec299e89f4a14
brader0 <brader01@luther.edu>
2015-10-19 14:27:21 -0500

    Update finalproject.js

--- finalproject.js
+++ finalproject.js
@@ -1,13 +1,6 @@
 addTask = function() {
-  article = document.querySelector("theArticle");
-  uL = document.createElement("ul");
+  uL = document.querySelector("#theList");
   newLi = document.createElement("li");
-  lIcontents = document.createTextNode(This is first");
-        li2 = document.createElement("li");
-        li2contents = document.createTextNode("This is second");
-        li2.appendChild(li2contents);
-        li1.appendChild(li1contents);
-        li1.appendChild(li2);
-        oL.appendChild(li1);
-        body.appendChild(oL);
-      }
+  newLi.className = priority.value
+  newLicontents = document.createTextNode("#theTask.value");
+}
