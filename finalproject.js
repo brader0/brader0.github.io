26ae377e2a59d7f5b7d2a0565e06fc75e0b2ebe9
brader0 <brader01@luther.edu>
2015-10-19 20:26:49 -0500

    Update finalproject.js

--- finalproject.js
+++ finalproject.js
@@ -1,13 +1,13 @@
 addTask = function() {
   var uL
   var task
-  task = document.querySelector('#theTask.value');
   uL = document.querySelector('#theList');
+  task = document.querySelector('#theTask').value;
+  checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
   newLi = document.createElement('li');
   liContents = document.createTextNode(task);
-  newLi.appendChild(liContents);
-  checkBox = document.createElement('input');
-  checkBox.type = checkbox
   newLi.appendChild(checkBox);
+  newLi.appendChild(liContents);
   uL.appendChild(newLi);
 }
