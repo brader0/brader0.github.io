37754d23df4503e966e0b46c331e12e2bf76a2f2
brader0 <brader01@luther.edu>
2015-10-21 13:43:19 -0500

    Update finalproject.js

--- finalproject.js
+++ finalproject.js
@@ -15,6 +15,7 @@ addTask = function() {
   color = document.querySelector('#priority').value;
   checkBox = document.createElement('input');
   checkBox.type = 'checkbox';
+  checkBox.onclick = doneTask;
   newLi = document.createElement('li');
   newLi.className = color;
   liContents = document.createTextNode(task);
